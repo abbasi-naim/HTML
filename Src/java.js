8269fce3d66a0c67286e91f6e2dff71945239c8e
Abbasi-Naim <abbasi.naim@gmail.com>
2020-09-23 15:18:11 +0330

    Import and Export Modules

--- Src/java.js
+++ Src/java.js
@@ -717,6 +717,16 @@ for (let v of set.values())
  }, 1000);
 
 
+ //Import und Export 
+ //import the following from "util/calc.js"
+ export const hit = (x, y, z) => {
+    return x * y + z / 2;
+  }
+  export const degree = 50;
+
+import * as calc from "util/calc";
+calc.hit(1,2,calc.degree);
+
 
 
 
