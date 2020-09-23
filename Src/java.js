80acff83db3d2a4395811b73a264f1520661e6b9
Abbasi-Naim <abbasi.naim@gmail.com>
2020-09-23 15:37:43 +0330

    Example of ...arr,...arr

--- Src/java.js
+++ Src/java.js
@@ -740,6 +740,13 @@ console.log("foo".repeat(3)); // foofoofoo
 "SoloLearn".includes("olo", 1); // true
 "SoloLearn".includes("olo", 2); // false
 
+//Example make the variable arr3 look like the following: [1, 2, 3, 4, 5, 6, 7, 8]
+
+const arr1 =[1,2,3];
+const arr2 = [5,6,7,8];
+let arr3 = [...arr,4,...arr2];
+ document.write(arr3);
+
 
 
 
